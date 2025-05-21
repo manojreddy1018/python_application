ade70557a5825b5c41d0a30dab0a56720a18ecd9
manu <121549642+manojreddy1018@users.noreply.github.com>
2025-05-21 11:15:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello from Azure DevOps Python App!'
+def home():
+    return 'Application deployed successfully'
 
 if __name__ == '__main__':
+    # Run the app on Unix/Linux systems, listening on all interfaces
     app.run(host='0.0.0.0', port=5000)
